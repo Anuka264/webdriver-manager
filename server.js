2d62a7a34a86d2e69edfedb57d9a2339dd7f4b86
Anuka <Anuka@Baku.localdomain>
2026-09-11 08:55:55 +0000

    Add package.json and server.js for Docker build

---
+++ server.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Hello from Exam App!'));
+app.listen(3003, () => console.log('Server running on port 3003'));
